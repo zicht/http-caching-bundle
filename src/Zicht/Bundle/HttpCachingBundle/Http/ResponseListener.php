f5a866365f32882d8a239a7415d7eb5c53f5086f
Gerard van Helden <gerard@zicht.nl>
2015-10-19 19:24:27 +0000

    fixes an issue where the session was not cleared if complex empty objects or arrays were in it
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtHttpCachingBundle/trunk@71100 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/HttpCachingBundle/Http/ResponseListener.php
+++ src/Zicht/Bundle/HttpCachingBundle/Http/ResponseListener.php
@@ -42,7 +42,7 @@ class ResponseListener
         }
 
         if ($event->getRequest()->hasSession() && $event->getRequest()->getSession()->isStarted()) {
-            if (array() === $event->getRequest()->getSession()->all()) {
+            if ($this->isEmpty($event->getRequest()->getSession()->all())) {
                 $event->getRequest()->getSession()->clear();
                 $event->getRequest()->cookies->remove(session_name());
                 $event->getResponse()->headers->clearCookie(session_name());
@@ -63,5 +63,27 @@ class ResponseListener
     protected function optimize(Request $request, Response $response)
     {
         $this->responseOptimizer->optimize($request, $response);
+    }
+
+
+    /**
+     * Returns true if the session may be considered "empty", i.e. all vars within the session are empty, or it
+     * is an array or object consisting of only empty values.
+     *
+     * @param $vars
+     * @return bool
+     */
+    protected function isEmpty($vars)
+    {
+        $isEmpty = false;
+        if (empty($vars)) {
+            $isEmpty = true;
+        } elseif (is_object($vars) || is_array($vars)) {
+            $isEmpty = true;
+            foreach ($vars as $key => $value) {
+                $isEmpty &= $this->isEmpty($value);
+            }
+        }
+        return $isEmpty;
     }
 }
